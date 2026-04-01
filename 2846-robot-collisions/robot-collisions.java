37929bb83d6a0daa86232de6a4499269fb57dfea
Hari-Jod <ingurhari9@gmail.com>
2026-04-01 22:29:35 +0530

    Time: 26 ms (96.52%) | Memory: 109.9 MB (91.30%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        int n = positions.length;
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+        Arrays.sort(indices, (a, b) -> Integer.compare(positions[a], positions[b]));
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i : indices) {
+            if (directions.charAt(i) == 'R') {
+                stack.push(i);
+            } else {
+                while (!stack.isEmpty() && healths[i] > 0) {
+                    int top = stack.peek();
+                    if (healths[i] > healths[top]) {
+                        healths[i] -= 1;
+                        healths[top] = 0;
+                        stack.pop();
+                    } else if (healths[i] < healths[top]) {
+                        healths[top] -= 1;
+                        healths[i] = 0;
+                    } else {
+                        healths[i] = 0;
+                        healths[top] = 0;
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int h : healths) {
+            if (h > 0) {
+                result.add(h);
+            }
+        }
+        return result;
+    }
+}
