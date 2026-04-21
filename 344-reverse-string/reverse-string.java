299c7ecd605a04290fa25646e451faac0edd8a5f
Hari-Jod <ingurhari9@gmail.com>
2026-04-21 14:40:29 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (76.97%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -3,9 +3,9 @@ class Solution {
      int n = s.length;
      int i = 0;   
      int j = n - 1;
-     char temp;
+    //  char temp;
      while(i < j){
-        temp = s[i];
+        char temp = s[i];
         s[i] = s[j];
         s[j] = temp;
         i++;
