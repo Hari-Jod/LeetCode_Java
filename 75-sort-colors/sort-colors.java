c8e47edfa27984c430125225ed5f41998155f28c
Hari-Jod <ingurhari9@gmail.com>
2026-04-24 10:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (44.97%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,25 +1,54 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int ones = 0;
-        int twos = 0;
-        int zeroes = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                zeroes++;
-            } else if (nums[i] == 1) {
-                ones++;
-            } else
-                twos++;
-        }
-        int b = 0;
-        while (b < n) {
-            if (b < zeroes) {
-                nums[b++] = 0;
-            } else if (b < zeroes + ones) {
-                nums[b++] = 1;
+        // int ones = 0;
+        // int twos = 0;
+        // int zeroes = 0;
+        // int n = nums.length;
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] == 0) {
+        //         zeroes++;
+        //     } else if (nums[i] == 1) {
+        //         ones++;
+        //     } else
+        //         twos++;
+        // }
+        // int b = 0;
+        // for(int i = 0;i < zeroes;i++){
+        //     nums[b++] = 0;
+
+        // }
+        // for(int i = 0;i < ones;i++){
+        //     nums[b++] = 1;
+
+        // }
+        // for(int i = 0;i < twos;i++){
+        //     nums[b++] = 2;  
+        // }
+        // while (b < n) {
+        //     if (b < zeroes) {
+        //         nums[b++] = 0;
+        //     } else if (b < zeroes + ones) {
+        //         nums[b++] = 1;
+        //     } else {
+        //         nums[b++] = 2;
+        //     }
+        // }
+
+        int low = 0, mid = 0,high = nums.length-1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                mid++;
+                low++;
+            } else if(nums[mid] == 1){
+                mid++;
             } else {
-                nums[b++] = 2;
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
             }
         }
     }
