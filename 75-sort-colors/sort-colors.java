993f1dabbeff028e4f15a003c3b40ead4989d022
Hari-Jod <ingurhari9@gmail.com>
2026-04-24 09:55:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (78.98%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -15,15 +15,15 @@ class Solution {
         int b = 0;
         for(int i = 0;i < zeroes;i++){
             nums[b++] = 0;
-            // b++;
+
         }
         for(int i = 0;i < ones;i++){
             nums[b++] = 1;
-            // b++;
+
         }
         for(int i = 0;i < twos;i++){
             nums[b++] = 2;
-            // b++;
+          
         }
        
     }
