99c533a93d2eba69cf539e3fb3f815a713196db8
Hari-Jod <ingurhari9@gmail.com>
2026-04-27 23:17:36 +0530

    Time: 17 ms (80.09%) | Memory: 88.5 MB (84.25%) - LeetSync

---
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    private int[][][] drive = {
+        {}, 
+        {{0, -1}, {0, 1}},  
+        {{-1, 0}, {1, 0}}, 
+        {{0, -1}, {1, 0}},  
+        {{0, 1}, {1, 0}},   
+        {{0, -1}, {-1, 0}},
+        {{0, 1}, {-1, 0}}   
+    };
+
+    public boolean hasValidPath(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+
+        queue.add(new int[]{0, 0});
+        visited[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int r = curr[0], c = curr[1];
+            if (r == m - 1 && c == n - 1) return true;
+
+            int type = grid[r][c];
+            for (int[] dir : drive[type]) {
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
+                    if (canConnect(grid[nr][nc], -dir[0], -dir[1])) {
+                        visited[nr][nc] = true;
+                        queue.add(new int[]{nr, nc});
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    private boolean canConnect(int type, int targetR, int targetC) {
+        for (int[] dir : drive[type]) {
+            if (dir[0] == targetR && dir[1] == targetC) return true;
+        }
+        return false;
+    }
+}
