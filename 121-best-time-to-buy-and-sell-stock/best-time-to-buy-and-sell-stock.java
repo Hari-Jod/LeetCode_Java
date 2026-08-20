9739c64d01bd69985f5633296993700f748c2069
SRIHARI INGURTHI <ingurhari9@gmail.com>
2026-08-20 21:41:21 +0530

    Time: 1 ms (99.96%) | Memory: 94.3 MB (86.28%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // int max = Integer.MIN_VALUE;
-        // for(int i = prices.length - 1; i >= 0; i--){
-        //     for(int j = prices.length - 1; j >= 0; j--){
-        //         max = Math.max(max, (prices[j] - prices[1])*(-1));
-        //     }
-        // }
-        // return max;
-        int mxp = 0, mnc = prices[0];
-        for(int i = 0; i < prices.length; i++){
-            mxp = Math.max(mxp, prices[i] - mnc);
+        int max = 0, mnc = Integer.MAX_VALUE;
+        for( int i = 0; i < prices.length; i++) {
+            max = Math.max(max, prices[i] - mnc);
             mnc = Math.min(mnc, prices[i]);
         }
-        return mxp;
+        return max;
     }
 }
