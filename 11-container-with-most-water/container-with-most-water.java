4cf20af7f3811301f06ae036f04cde434577e6af
SRIHARI INGURTHI <ingurhari9@gmail.com>
2026-08-25 21:15:52 +0530

    Time: 4 ms (94.95%) | Memory: 77.1 MB (85.89%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,11 +1,10 @@
 class Solution {
     public int maxArea(int[] height) {
-        int n = height.length;
         int i = 0;
-        int j = n - 1;
+        int j = height.length - 1;
         int max = 0;
+        int area  = 0;
         while(i < j) {
-            int area  = 0;
             if(height[i] < height[j]){
                 area = height[i] * (j - i);
                 i++;
