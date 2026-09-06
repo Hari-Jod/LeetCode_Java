f782d045fa98846fdecc700afdb79a875a5df938
SRIHARI INGURTHI <ingurhari9@gmail.com>
2026-09-06 13:03:32 +0530

    Time: 17 ms (81.61%) | Memory: 42.9 MB (90.46%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,28 +1,26 @@
 class Solution {
-    public int solve(int i, int j, String s, String t, int[][] memo) {
-        if(j==t.length())
-            return 1;
-        if(i == s.length())
-            return 0;
-        if(memo[i][j] != -1)
-            return memo[i][j];
-        int notTake = solve(i+1, j, s, t,memo);
-        int take = 0;
-
-        if(s.charAt(i) == t.charAt(j)){
-            take = solve(i + 1, j + 1, s, t,memo);
-        }
-
-        return memo[i][j] = take + notTake;
-    }
     public int numDistinct(String s, String t) {
-        int n = s.length();
-        int m = t.length();
+            int m = s.length();
+            int n = t.length();
 
-        int[][] memo = new int[n][m];
-        for(int[] row : memo)
-            java.util.Arrays.fill(row, -1);
+            int[] dp = new int[n+ 1];
 
-        return solve(0,0,s,t,memo);
+            for(int i = 1; i <= m; i++) {
+                int last = 1;
+
+                for (int j = 1; j <= n;j++) {
+                    int cur = dp[j];
+
+                    int take = 0, notTake;
+                    if(s.charAt(i - 1) == t.charAt(j-1)){
+                        take = last;
+                    }
+                    notTake = cur;
+
+                    dp[j] = take + notTake;
+                    last = cur;
+                }
+            }
+                return dp[n];
     }
 }
