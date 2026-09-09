9debbe18c3ba8d3c38c89bbcf6f8c98696d50c55
SRIHARI INGURTHI <ingurhari9@gmail.com>
2026-09-10 00:46:42 +0530

    Time: 1 ms (100.00%) | Memory: 48.5 MB (96.55%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,29 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int i = 0, j = 0, m1 = 0, m2 = 0;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
 
-        for(int count = 0;count <= (n + m)/2; count++) {
-            m2 = m1;
-            if(i != n && j != m) {
-                if(nums1[i] > nums2[j]) {
-                    m1 = nums2[j++];
-                } else {
-                    m1 = nums1[i++];
-                }
-            } else if(i < n) {
-                m1 = nums1[i++];
+        if(n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2;
+        int low = 0, high = n1;
+
+        while(low <= high) {
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
+
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE, r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+
+            if(mid1 < n1) 
+                r1 = nums1[mid1];
+            if(mid2 < n2)
+                r2 = nums2[mid2];
+            if(mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+
+            if(l1 <= r2 && l2 <= r1) {
+                if(n % 2 == 1)
+                    return Math.max(l1, l2);
+                else
+                    return ((double)(Math.max(l1,l2) + Math.min(r1, r2)))/2.0;
+            } else if(l1 > r2) {
+                high = mid1 - 1;
             } else {
-                m1 = nums2[j++];
+                low = mid1 + 1;
             }
         }
-
-        if((n + m) % 2 == 1) {
-            return (double)m1;
-        } else {
-            double ans = (double) m1 + (double)m2;
-            return ans/2.0;
-        }
+        return 0;
     }
 }
