6013e4dae21de5d369aaf2c9fce68986856f4800
SRIHARI INGURTHI <ingurhari9@gmail.com>
2026-09-16 18:23:35 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (21.59%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -4,14 +4,11 @@ class Solution {
         int j = s.length - 1;
 
         while(i < j) {
-
             char temp = s[i];
-            s[i] =  s[j];
+            s[i] = s[j];
             s[j] = temp;
             i++;
             j--;
-        }
-
-
+        }   
     }
 }
