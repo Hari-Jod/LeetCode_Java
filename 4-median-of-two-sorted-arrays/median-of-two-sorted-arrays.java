08a9f287ced36b118e72938b98dacc9e91ba5d06
SRIHARI INGURTHI <ingurhari9@gmail.com>
2026-09-24 13:53:48 +0530

    Time: 7 ms (23.33%) | Memory: 49 MB (32.30%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,39 +1,21 @@
+import java.util.Arrays;
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
+        int n = nums1.length;
+        int m = nums2.length;
+        int[] merged = new int[n + m];
+        int k = 0;
+        for(int i = 0; i < n; i++) 
+            merged[k++] = nums1[i];    
+        for(int i = 0; i < m; i++) 
+            merged[k++] = nums2[i];
+        Arrays.sort(merged);
 
-        if(n1 > n2)
-            return findMedianSortedArrays(nums2,nums1);
-        int n = n1 + n2;
-        int left = (n1 + n2 + 1)/2;
-        int low = 0, high = n1;
-        while(low <= high) {
-            int mid1 = (low + high) >> 1;
-            int mid2 = left - mid1;
+        int total = merged.length;
 
-            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE, r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
-
-            if(mid1 < n1)
-                r1 = nums1[mid1];
-            if(mid2 < n2)
-                r2 = nums2[mid2];
-            if(mid1 - 1 >= 0)
-                l1 = nums1[mid1 - 1];
-            if(mid2 - 1 >= 0)
-                l2 = nums2[mid2 - 1];
-            
-            if(l1 <= r2 && l2 <= r1) {
-                if(n%2 == 1)
-                    return Math.max(l1,l2);
-                else
-                    return ((double)(Math.max(l1,l2) + Math.min(r1,r2)))/2.0;
-            } else if(l1 > r2) {
-                high = mid1 - 1;
-            } else {
-                low = mid1 + 1;
-            }
-        }
-        return 0;
+        if(total % 2 == 1) 
+            return (double) merged[total/2];
+        else 
+            return (double)((merged[total/2 - 1] + merged[total/2]) / 2.0);
     }
 }
